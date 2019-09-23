9b68ce6ab87db250ec90f65b49b1d453d87204f3
patoattie <patoattie@gmail.com>
2019-09-23 00:08:11 -0300

    Haciendo aplicacion base con login y registro

--- app-basica/src/app/componentes/login/login.component.ts
+++ app-basica/src/app/componentes/login/login.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
+//import { Observable } from 'rxjs';
 import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 
 import { Usuario } from '../../clases/usuario';
@@ -12,27 +12,23 @@ import { LoginService } from '../../servicios/login.service';
 })
 export class LoginComponent implements OnInit 
 {
-  //public correo: string;
-  //public clave: string;
   public ok: boolean;
   public error: boolean;
   private formulario: FormGroup;
   private usuarios: Usuario[];
   public errorDatos: boolean;
-
+  
   constructor(private loginService: LoginService, formBuilder: FormBuilder) 
   {
     this.formulario = formBuilder.group(
       {
         correo: ['', Validators.compose([Validators.pattern("^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$"), Validators.required])],
-        clave: ['', Validators.compose([Validators.required])]
+        clave: ['', Validators.compose([Validators.minLength(4), Validators.required])]
       });
   }
 
   ngOnInit() 
   {
-    //this.correo = '';
-    //this.clave = '';
     this.ok = false;
     this.error = false;
     this.errorDatos = false;
@@ -52,15 +48,11 @@ export class LoginComponent implements OnInit
     let retorno: boolean = false;
     this.usuarios.forEach(unUsuario =>
     {
-      //if(unUsuario.correo == this.correo && unUsuario.clave == this.clave)
       if(unUsuario.correo == this.formulario.value.correo && unUsuario.clave == this.formulario.value.clave)
       {
           retorno = true;
       }
     });
-
-    /*this.error = !(retorno);
-    this.ok = retorno;*/
 
     return retorno;
   }
